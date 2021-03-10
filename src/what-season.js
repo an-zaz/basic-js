16afd227dc9a6e5c776bba1807a485b7f094b193
an-zaz <an_zazerskaya@mail.ru>
2021-03-10 19:54:12 +0300

    feat: finish what-season task

--- src/what-season.js
+++ src/what-season.js
@@ -1,20 +1,25 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(date) {
-  if (!date) {
-    return 'Unable to determine the time of year!';
-  }
-  let month = date.getMonth();
-  if ((month >= 0 && month <= 1) || month === 11) {
-    return 'winter';
-  }
-  if (month >= 2 && month <= 4) {
+function getSeason(date) {
+ if (!arguments.length){
+   return 'Unable to determine the time of year!';
+ }
+ if (date.getMonth !== Date.prototype.getMonth) {
+   throw new Error();
+ }
+ let month = date.getMonth();
+ if (month === 11 || month === 0 || month === 1) {
+   return 'winter';
+ }
+ if (month >= 2 && month <= 4) {
     return 'spring';
   }
-  if (month >= 5 && month <= 7) {
+ if (month >= 5 && month <= 7) {
     return 'summer';
   }
-  if (month >= 8 && month <= 10) {
-    return 'fall';
+ if (month  >= 8 && month <= 10) {
+    return 'autumn';
   }
 };
+
+module.exports = getSeason;
