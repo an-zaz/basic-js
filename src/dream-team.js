a0c4d1be678f86bf2769ee2f36a0f9ce3b08d9fa
an-zaz <an_zazerskaya@mail.ru>
2021-03-10 19:53:38 +0300

    feat: finish dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,17 +1,45 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(members) {
-  if (!Array.isArray(members)) {
+function createDreamTeam(members) {
+  if ( !Array.isArray(members)){
     return false;
   }
-  return members
-    .map(member => {
-      if (typeof member !== 'string') {
-        return false;
-      }
-      return member.trim()[0].toUpperCase()
-    })
-    .filter(item => item)
-    .sort()
-    .join('')
-};
+  let name = [];
+  for (let item of members) {
+    if ( typeof item === 'string'){
+      name.push(item.trim()[0].toUpperCase());
+    }
+  }
+  return name.sort().join('');
+}
+
+module.exports =  createDreamTeam;
+
+console.log(createDreamTeam([
+  'Amelia',
+  null,
+  undefined,
+  'Ruby',
+  'Lily',
+  11,
+  'Grace',
+  22,
+  'Millie',
+  'Daisy',
+  true,
+  'Freya',
+  false,
+  'Erin',
+  new Set([1,2,3,4,5]),
+  'Megan',
+  {
+    'John': 'Smith'
+  },
+  'Jasmine',
+  1,
+  2,
+  3,
+  4,
+  5,
+  'Brooke',
+]));
