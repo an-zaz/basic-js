172d6f022001bba0a6a7799b1ec9b439f54ea799
an-zaz <an_zazerskaya@mail.ru>
2021-03-15 23:49:46 +0300

    feat: solve simple-chain task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,44 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  arr: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (arguments.length) {
+      this.arr[this.arr.length] = value;
+    } else {
+      this.arr[this.arr.length] = '';
+    }
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if ( typeof position !== 'number' && !Number.isFinite(position) ){
+      this.cleanChain();
+      throw new Error();
+    }
+    this.arr.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.arr.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let str = this.arr.map( function (item) {
+      return `( ${item} )~~`
+    }).join('');
+    this.cleanChain();
+    return  str.slice(0, str.length - 2);
+  },
+  cleanChain() {
+    this.arr = [];
   }
 };
 
 module.exports = chainMaker;
+console.log(
+  chainMaker.addLink('GHI').addLink(null).reverseChain().addLink(333).reverseChain().reverseChain().addLink(0).reverseChain().reverseChain().addLink('GHI').finishChain(), '( null )~~( GHI )~~( 333 )~~( 0 )~~( GHI )'
+);
+
